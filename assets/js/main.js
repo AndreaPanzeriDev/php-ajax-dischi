60feb122c71a9e3d4f1ffcfa7754c2a30110bfb0
AndreaPanzerDev <andpanz@libero.it>
2022-12-15 14:49:45 +0100

    auto-save

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        arrayDischi: [],
+    },
+    mounted() {
+        axios.get('../api/index.php').then((response) => {
+            console.log(response.data);
+            this.arrayDischi = response.data;
+
+        })
+    },
+    methods: {
+
+    }
+
+})
